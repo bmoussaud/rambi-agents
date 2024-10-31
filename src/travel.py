bb50fe3cadc881624e23f603f0ee6f84177ff671
Benoit Moussaud <benoit@moussaud.org>
2024-10-31 11:47:05 +0100

    travel agent

---
+++ src/travel.py
@@ -0,0 +1,74 @@
+from autogen_agentchat.agents import CodingAssistantAgent
+from autogen_agentchat.teams import MaxMessageTermination, StopMessageTermination
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_ext.models import AzureOpenAIChatCompletionClient
+
+from dotenv import load_dotenv
+import logging
+import asyncio
+
+from autogen_agentchat import EVENT_LOGGER_NAME
+from autogen_agentchat.logging import ConsoleLogHandler
+from autogen_agentchat.teams import MaxMessageTermination
+from autogen_ext.models import AzureOpenAIChatCompletionClient
+
+logger = logging.getLogger(EVENT_LOGGER_NAME)
+logger.addHandler(ConsoleLogHandler())
+logger.setLevel(logging.INFO)
+
+# Get configuration settings
+load_dotenv()
+
+async def main():
+    # Create an OpenAI model client.
+    model_client = AzureOpenAIChatCompletionClient(
+        azure_endpoint="https://27iigguorarqw-openai.openai.azure.com/",
+        azure_deployment="gpt4o/chat/completions?api-version=2024-08-01-preview",
+        model="gpt-4o-2024-08-06",
+        api_version="2024-08-01-preview",
+        model_capabilities={
+            "vision": False,
+            "audio": False,
+            "json_output": True,
+            "chat": True,
+            "function_calling": True},
+        )
+
+    planner_agent = CodingAssistantAgent(
+        "planner_agent",
+        model_client=model_client,
+        description="A helpful assistant that can plan trips.",
+        system_message="You are a helpful assistant that can suggest a travel plan for a user based on their request.",
+    )
+
+    local_agent = CodingAssistantAgent(
+        "local_agent",
+        model_client=model_client,
+        description="A local assistant that can suggest local activities or places to visit.",
+        system_message="You are a helpful assistant that can suggest authentic and interesting local activities or places to visit for a user and can utilize any context information provided.",
+    )
+
+    language_agent = CodingAssistantAgent(
+        "language_agent",
+        model_client=model_client,
+        description="A helpful assistant that can provide language tips for a given destination.",
+        system_message="You are a helpful assistant that can review travel plans, providing feedback on important/critical tips about how best to address language or communication challenges for the given destination. If the plan already includes language tips, you can mention that the plan is satisfactory, with rationale.",
+    )
+
+    travel_summary_agent = CodingAssistantAgent(
+        "travel_summary_agent",
+        model_client=model_client,
+        description="A helpful assistant that can summarize the travel plan.",
+        system_message="You are a helpful assistant that can take in all of the suggestions and advice from the other agents and provide a detailed tfinal travel plan. You must ensure th b at the final plan is integrated and complete. YOUR FINAL RESPONSE MUST BE THE COMPLETE PLAN. When the plan is complete and all perspectives are integrated, you can respond with TERMINATE.",
+    )
+
+    group_chat = RoundRobinGroupChat([planner_agent, local_agent, language_agent, travel_summary_agent])
+    result = await group_chat.run(task="Plan a 3 day trip to Tokyo.", termination_condition=StopMessageTermination())
+    #print(result)
+    # Access the messages attribute directly
+    for message in result.messages:
+        print(message.content)
+        print("-----\n")
+
+
+asyncio.run(main())
